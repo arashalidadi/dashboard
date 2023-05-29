05f2dda36d2a22d3d4cdc92f85e4a18602e59044
Arash@m <alidadiarash71@gmail.com>
2023-05-29 19:09:56 +0430

    Add Setting Tooltip Buttom

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import React from 'react'
 import './App.css'
+import {BrowserRouter , Router ,Route} from 'react-router-dom'
+import {TooltipComponent} from '@syncfusion/ej2-react-popups'
+import {FiSettings} from 'react-icons/fi'
 const App = () => {
   return (
-    <h1 className="text-9xl font-bold underline text-green-600">App aa</h1>
+   <div>
+    <BrowserRouter>
+      <div className='flex relative dark:bg-main-dark-bg' >
+         <div className='fixed bottom-4 right-4' style={{zIndex:'1000'}}>
+           <TooltipComponent content='Settings' position='Top'>
+             <button type='button' className='text-3xl p-3 
+             hover:drop-shadow-xl hover:bg-light-gray text-white'
+             style={{background:'blue',borderRadius:'50%' }}>
+              <FiSettings />
+             </button>
+           </TooltipComponent>
+         </div>
+      </div>
+    </BrowserRouter>
+   </div>
   )
 }
 
