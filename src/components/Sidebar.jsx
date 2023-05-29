0bdb145e1af190c9d70ec3f2441c6fce161c12ed
Arash@m <alidadiarash71@gmail.com>
2023-05-29 22:30:48 +0430

    Map the constant off names and icons to the sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import { SiShopware } from 'react-icons/si'
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 import {MdOutlineCancel} from 'react-icons/md'
+import {links} from '../data/dummy'
 const Sidebar = () => {
   const activeMenu = true;
+  const activeLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg  text-white  text-md m-2';
+  const normalLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2';
 
   return (
     <div className='ml-3 h-screen 
@@ -29,8 +32,27 @@ const Sidebar = () => {
           </button>
        </TooltipComponent>
      </div>
-      <div>
-        
+      <div className='mt-10'>
+      {links.map((item) =>(
+        <div key={item.title}>
+          <p className='text-gray-400 m-3 mt-4 uppercase'>
+          {item.title}
+          </p>
+          {item.links.map((link) =>(
+            <NavLink
+            to={`/${link.name}`}
+            key={link.name}
+            onClick={() =>{}}
+            className={({isActive}) => isActive ? activeLink : normalLink}
+            >
+              {link.icon}
+              <span className='capitalize'>
+                {link.name}
+              </span>
+            </NavLink>
+          ))}
+        </div>
+      ))}
       </div>
      </>) }
     </div>
