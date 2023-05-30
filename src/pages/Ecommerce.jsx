b1bc6d83172b967adbc9537edbfb81ff26797278
Arash@m <alidadiarash71@gmail.com>
2023-05-30 18:29:50 +0430

    Map the earningData on the ecommerce page

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Button } from "../components";
-
+import { earningData } from "../data/dummy";
 const Ecommerce = () => {
   return (
     <div className="mt-12">
@@ -26,6 +26,35 @@ const Ecommerce = () => {
               size="md"
             />
           </div>
+        </div>
+        <div
+          className="flex m-3 flex-wrap justify-center 
+        gap-1 items-center"
+        >
+          {earningData.map((item) => (
+            <div
+              key={item.title}
+              className="bg-white dark:bg-secondary-dark-bg
+             md:w-56 p-4 pt-9 rounded-2xl"
+            >
+              <button
+                type="button"
+                style={{ color: item.color, backgroundColor: item.iconBg }}
+                className="text-2xl opacity-0.9 hover:drop-shadow-xl"
+              >
+                {item.icon}
+              </button>
+              <p className="mt-3">
+                <span className="text-lg font-semibold">{item.amount}</span>
+                <span className={`text-sm text-${item.pcColor} ml-2`}>
+                  {item.percentage}
+                </span>
+              </p>
+              <p className="text-sm text-gray-400 mt-1">
+                {item.title}
+              </p>
+            </div>
+          ))}
         </div>
       </div>
     </div>
