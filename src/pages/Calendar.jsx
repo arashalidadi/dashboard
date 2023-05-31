99423adad34488143e5466e1d8c1c5acf4abbde8
Arash@m <alidadiarash71@gmail.com>
2023-05-31 05:23:42 +0430

    create calendar page in app title

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from "react";
+import { DatePickerComponent, Inject } from "@syncfusion/ej2-react-calendars";
+import {
+  Agenda,
+  Day,
+  DragAndDrop,
+  Month,
+  Resize,
+  ScheduleComponent,
+  ViewDirective,
+  ViewsDirective,
+  Week,
+  WorkWeek,
+} from "@syncfusion/ej2-react-schedule";
+import { scheduleData } from "../data/dummy";
+import { Header } from "../components";
 
 const Calendar = () => {
   return (
-    <div>Calendar</div>
-  )
-}
+    <div
+      className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white
+    rounded-3xl"
+    >
+      <Header category="App" title="Calendar" />
+      <ScheduleComponent
+        height="650px"
+        eventSettings={{ dataSource: scheduleData }}
+      >
+        <Inject
+          services={[Day, Week, WorkWeek, Month, Agenda, Resize, DragAndDrop]}
+        />
+      </ScheduleComponent>
+    </div>
+  );
+};
 
-export default Calendar
+export default Calendar;
